0ca4892acba37d5f474166b835bf924bf3569beb
Veniamin <venia@veselovsky.org>
2022-12-14 15:45:45 +0100

    data

--- app.py
+++ app.py
@@ -5,13 +5,15 @@ import pandas as pd
 
 app = Dash(__name__)
 
-df = pd.read_csv("/ada1/projects/socialknowledge/data/article_reddit-wiki_partisan_scores_old.csv")
+df = pd.read_csv("/ada1/projects/socialknowledge/src/webpage/data/article_reddit-wiki_partisan_scores_old.csv")
 
 
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
 
-fig = px.scatter(df, x="partisan_reddit", y="score", color="GS", hover_data=["page_title","GS"])
+fig = px.scatter(df, x="partisan_reddit", y="score", color="number_editors", hover_data=["page_title","GS"],
+        labels=dict(partisan_reddit="Reddit partisan score", score="Wikipedia partisan score", number_editors="Number Editors"),height=600
+    )
 
 app.layout = html.Div(children=[
     html.Div(children='''
@@ -55,15 +57,16 @@ fig.update_yaxes(
     Input("range-slider2", "value"),
     Input("range-slider3", "value")])
 def update_bar_chart(slider_range,slider_range2,slider_range3):
-    df = pd.read_csv("/ada1/projects/socialknowledge/data/article_reddit-wiki_partisan_scores_old.csv")
+    df = pd.read_csv("/ada1/projects/socialknowledge/src/webpage/data/article_reddit-wiki_partisan_scores_old.csv")
     low, high = slider_range
     low2,high2=slider_range2
     low3,high3=slider_range3
 
     mask = (df['number_commenters'] > low) & (df['number_commenters'] < high)& (df['partisan_neutral'] < high2) & (df['partisan_neutral'] > low2) & (df['GS'] < high3) & (df['GS'] > low3) 
     fig = px.scatter(
-        df[mask], x="partisan_reddit", y="score", color="GS",
-        hover_data=['page_title',"GS"])
+        df[mask], x="partisan_reddit", y="score", color="number_editors",
+        hover_data=['page_title',"GS"],
+        labels=dict(partisan_reddit="Reddit partisan score", score="Wikipedia partisan score", number_editors="Number Editors"), height = 600)
     return fig
 
 if __name__ == '__main__':
